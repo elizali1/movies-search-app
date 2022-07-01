5120481b39f3a8c29ed64e5fbce17170c3551dc9
Eliza Li <lie2@bxscience.edu>
2022-07-01 14:51:27 -0400

    Movie search app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import MovieInfo from './components/MovieInfo';
+import {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state= {
+    baseURL:'http://www.omdbapi.com/?',
+    apiKey:'apikey=' + 'acc961aa',
+    query:'&t=',
+    movieTitle:'',
+    searchURL:'',
+    movie: null
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({searchURL: this.state.baseURL + this.state.apiKey + this.state.query + this.state.movieTitle}, () => {
+      console.log(this.state.searchURL)
+      fetch(this.state.searchURL)
+      .then(res => res.json())
+      .then(json=>this.setState({movie: json}), err => console.log(err))
+    })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Movies App</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor='movieTitle'>Title</label>
+          <input
+          id='movieTitle'
+          type='text'
+          value={this.state.movieTitle}
+          onChange={this.handleChange} />
+          <input
+          type='submit' 
+          value='Find Movie Info'/>
+        </form>
+        <a href={this.state.searchURL}>{this.state.searchURL}</a>
+
+        {this.state.movie && <MovieInfo movie={this.state.movie} />}
+      </div>
+    )
+  }
 }
+
 
 export default App;
